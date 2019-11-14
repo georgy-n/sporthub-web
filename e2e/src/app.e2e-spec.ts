fbd66d43403d05273d44c460048a61b1ab1d4e87
Gosha20 <gosha3548@gmail.com>
2019-11-14 22:57:21 +0500

    prepare to delete component

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to web-bank!');
+    expect(page.getTitleText()).toEqual('Welcome to onlineshop-web!');
   });
 });
